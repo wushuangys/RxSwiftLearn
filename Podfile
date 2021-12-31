ab59ab68d1c8599d0d4fbd30d4afcc75a527123f
wushuang1 <wushuang_you@163.com>
2021-12-31 17:03:13 +0800

    创建项目

---
+++ Podfile
@@ -0,0 +1,10 @@
+platform :ios, '12.0'
+
+source 'https://github.com/CocoaPods/Specs.git'
+
+target 'RxSwiftLearnDemo' do
+  use_frameworks!
+
+  pod 'RxSwift'
+
+end
---
+++ RxSwiftLearnDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RxSwiftLearnDemo
+//
+//  Created by 无双 on 2021/12/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
